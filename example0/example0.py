61bf68131a16949f22d4674f8418a4fa32595c39
no3l <noel.garcia@openmailbox.org>
2019-04-11 11:36:26 +0200

    last minute changes

--- example0/example0.py
+++ example0/example0.py
@@ -6,11 +6,15 @@ import matplotlib.pyplot as plt
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
+#
+# to get rid of the TF compilation warnings
+#
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # to get rid of the TF warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-# Replicate the XOR behavior
+# Get the data
+# In this case we create it manually
 IN_train = np.array([[0,0],
                      [0,1],
                      [1,0],
@@ -21,10 +25,12 @@ OUT_train = np.array([[0],
                       [1],
                       [0]])
 
+# There is no test data, since the inuputs are constrained to $\{0,1\}$.
+# We use the training set as testing
 IN_test = IN_train
 OUT_test = OUT_train
 
-# Let's design the model
+# Try to load previously trained model
 try: model = keras.models.load_model('my_model.h5')
 except OSError:
    model = keras.Sequential([
@@ -40,10 +46,10 @@ print('Before training:')
 print('Input   xpct Out   Output')
 predicted = model.predict(IN_train)
 for i in range(IN_train.shape[0]):
-   print(IN_train[i],'   ',OUT_train[i],'     %.3f'%(predicted[i]))
+   print(IN_train[i],'   ',OUT_train[i],'      %.2f'%(predicted[i]))
 
 # Train the model
-history = model.fit(IN_train, OUT_train, epochs=1000,
+history = model.fit(IN_train, OUT_train, epochs=5000,
                     validation_data = (IN_test,OUT_test),
                     verbose=0)
 
@@ -61,7 +67,7 @@ print('After training:')
 print('Input    xpct Out      Output')
 predicted = model.predict(IN_train)
 for i in range(IN_train.shape[0]):
-   print(IN_train[i],'   ',OUT_train[i],'     %.3f'%(predicted[i]))
+   print(IN_train[i],'   ',OUT_train[i],'      %.2f'%(predicted[i]))
 plt.show()
 
 # Save result
